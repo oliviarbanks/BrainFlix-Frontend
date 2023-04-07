3f2d79a2a241eb7bd50c7cc6f3102b1903e9e57c
olivia <oliviarbanks@gmail.com>
2023-04-07 01:55:28 -0400

    linked main video scss page

--- src/components/Main/CommentsContainer/CommentsContainer.js
+++ src/components/Main/CommentsContainer/CommentsContainer.js
@@ -1,9 +1,11 @@
 function CommentsContainer({ videoData, videoIndex }) {
+  // comments.map(comment) was gonna put that in the parameter but didnt work
   console.log(videoData, "from comments container");
   return (
     <div>
-      <h1>This is the comments container component</h1>
-      <p className="video__comment">{videoData[videoIndex].comment}</p>
+      {/* key={comments.comment} found this online and said to insert in div */}
+      {/* <h1>This is the comments container component</h1> */}
+      <h3 className="video__comment">{videoData[videoIndex].comment}</h3>
     </div>
   );
 }
--- src/components/Main/VideoDetails/VideoDetails.js
+++ src/components/Main/VideoDetails/VideoDetails.js
@@ -2,7 +2,7 @@ function VideoDetails({ videoData, videoIndex }) {
   console.log(videoData, "from video details container");
   return (
     <div>
-      <h2 className="video__title">{videoData[videoIndex].title}</h2>
+      <h2 className="video__title">{videoData[videoIndex].description}</h2>
     </div>
   );
 }
