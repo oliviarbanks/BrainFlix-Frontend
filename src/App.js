6bed1b1cc471846b8857faf1d84d22dc1c187726
olivia <oliviarbanks@gmail.com>
2023-04-09 23:44:21 -0400

    final submission

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import MainVideo from "./components/Main/MainVideo/MainVideo";
 import SideBarVideosContainer from "./components/Main/SideBarVideosContainer/SideBarVideosContainer";
 import VideoDetails from "./components/Main/VideoDetails/VideoDetails";
 import videoData from "./data/video-details.json";
+import "./App.scss";
 
 function App() {
   const [videoIndex, setVideoIndex] = useState(0);
@@ -21,13 +22,19 @@ function App() {
     <div>
       <Header />
       <MainVideo videoData={videoData} videoIndex={videoIndex} />
-      <VideoDetails videoData={videoData} videoIndex={videoIndex} />
-      <CommentsContainer videoData={videoData} videoIndex={videoIndex} />
-      <SideBarVideosContainer
-        videoData={videoList}
-        videoIndex={videoIndex}
-        clickHandler={buttonClicked}
-      />
+      <div className="app">
+        <div className="app__commentDetails">
+          <VideoDetails videoData={videoData} videoIndex={videoIndex} />
+          <CommentsContainer videoData={videoData} videoIndex={videoIndex} />
+        </div>
+        <div className="app__videoDetails">
+          <SideBarVideosContainer
+            videoData={videoList}
+            videoIndex={videoIndex}
+            clickHandler={buttonClicked}
+          />
+        </div>
+      </div>
     </div>
   );
 }
