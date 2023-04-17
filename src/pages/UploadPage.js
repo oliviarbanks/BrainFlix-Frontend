7abb6f46eb5bd501f5e08410dd5bf638e63ca023
olivia <oliviarbanks@gmail.com>
2023-04-16 20:22:09 -0400

    upload page styling complete for all responsive widths

--- src/pages/UploadPage.js
+++ src/pages/UploadPage.js
@@ -6,36 +6,42 @@ function UploadPage() {
   return (
     <div className="upload">
       <h1 className="upload__header">Upload Video </h1>
-      <div className="upload__subcontainer">
-        <p className="upload__subheader">VIDEO THUMBNAIL</p>
-        <img
-          className="upload__image"
-          src={bikeImage}
-          alt="aerial view of riding a blue bike"
-        ></img>
-      </div>
-      <form className="upload__form">
-        <div className="upload__form-container">
-          <div className="upload__form-title">
-            <label className="upload__label" htmlFor="comment">
-              TITLE YOUR VIDEO
-            </label>
-            <input
-              className="upload__input-title"
-              placeholder="Add a title to your video"
-            ></input>
-          </div>
-          <div className="upload__form-description">
-            <label className="upload__label" htmlFor="comment">
-              ADD A VIDEO DESCRIPTION
-            </label>
-            <input
-              className="upload__input-description"
-              placeholder="Add a description to your video"
-            ></input>
-          </div>
+
+      <div className="upload__desktop">
+        <div className="upload__subcontainer">
+          <p className="upload__subheader">VIDEO THUMBNAIL</p>
+          <img
+            className="upload__image"
+            src={bikeImage}
+            alt="aerial view of riding a blue bike"
+          ></img>
         </div>
-      </form>
+        <div>
+          <form className="upload__form">
+            <div className="upload__form-container">
+              <div className="upload__form-title">
+                <label className="upload__label" htmlFor="comment">
+                  TITLE YOUR VIDEO
+                </label>
+                <input
+                  className="upload__input-title"
+                  placeholder="Add a title to your video"
+                ></input>
+              </div>
+              <div className="upload__form-description">
+                <label className="upload__label" htmlFor="comment">
+                  ADD A VIDEO DESCRIPTION
+                </label>
+                <input
+                  className="upload__input-description"
+                  placeholder="Add a description to your video"
+                ></input>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+
       <div className="upload__buttons">
         <button className="upload__publish">
           <img
